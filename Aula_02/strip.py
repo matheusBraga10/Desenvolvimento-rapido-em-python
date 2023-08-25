69d1ac13961450e54157d2525a4fa2c7a3cfe5b3
Matheus Felipe Braga <matheusfbraga10@gmail.com>
2023-08-24 23:41:37 -0300

    Create strip.py

---
+++ Aula_02/strip.py
@@ -0,0 +1,14 @@
+curso = '   Python '
+# Elimina todos espaçõs em branco
+print(curso.strip()+'.') 
+
+# Elimina espaçõs em branco da esquerda
+print(curso.lstrip()+'.')
+
+# Elimina espaçõs em branco da direita
+print(curso.rstrip()+'.')
+
+print()
+
+lista = ['linha','linha2','linha3','linha4','linha5']
+print(lista.strip()+', ')
