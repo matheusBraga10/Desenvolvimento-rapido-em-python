61235656ce2f9759f18e84f1fcf96cb81e0c5e09
Matheus Felipe Braga <matheusfbraga10@gmail.com>
2023-09-16 00:17:12 -0300

    16-09

--- Desafio/desafio.py
+++ Desafio/desafio.py
@@ -5,12 +5,24 @@ uma tabela no sqlite através de um programa escrito em Python. Os comandos SQL 
 O arquivo a ser utilizado será o Empresas0:
 '''
 
-import sqlite3 as conector
-import os
 import pandas as pd
+import requests 
+import json
+
+ENDERECO = '/home/matheus/Documentos/Empresas0/K3241.K03200Y0.D30812.EMPRECSV'
+RETORNO = 'https://sdw-2023-prd.up.railway.app/'
+
+df = pd.read_csv(ENDERECO)
+
+user_ids = df['id'].tolist()
+print(user_ids)
+
+def get_user(id):
+
+    response = requests.get(ENDERECO)
+    return response.json() if response.status_code == 200 else None
+
+user = [user for id in user_ids if (user := get_user(id)) is not None]
+print(json.dumps(user, ident=2))
 
 
-all_files = list(filter(lambda x:'.csv' in x, os.listdir('/home/matheus/Documentos/Empresas0/')))
-
-arquivo = []
-for 
