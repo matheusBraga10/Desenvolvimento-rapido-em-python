2eb853957a7669e0f6b0e84bb4188b84041c7d38
Matheus Felipe Braga <matheusfbraga10@gmail.com>
2023-10-20 00:43:45 -0300

    19-10

---
+++ trabalho_sistema_tkinter_db/Principal.py
@@ -0,0 +1,26 @@
+from naturezaJuridicaDAO import *
+from telaNaturezaJuridica import *
+import tkinter as tk
+
+if __name__ == '__main__':
+
+    janela = tk.Tk()
+    naturezaDAO = NaturezaJuridicaDAO()
+    telaNatureza = TelaNaturezaJuridica(janela, naturezaDAO) 
+    
+    
+    janela.mainloop()
+
+    # naturezaDAO = NaturezaJuridicaDAO()
+
+    # resultado = naturezaDAO.buscar()
+    # print(resultado)
+    # resultado = naturezaDAO.buscarPorCodigo(10000)
+    # # print(resultado)
+    # naturezaDAO.inserir(1, 'Nova Natureza')
+    # naturezaDAO.inserir(2, 'Nova Natureza 2')
+    # naturezaDAO.inserir(3, 'Nova Natureza 3')
+
+
+
+    
