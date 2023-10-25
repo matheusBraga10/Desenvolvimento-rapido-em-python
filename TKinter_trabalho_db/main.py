30d201ac2cc53fe41c922e0256915119a9b86eae
Matheus Felipe Braga <111787756+matheusBraga10@users.noreply.github.com>
2023-10-25 15:14:44 -0300

    25-10

--- TKinter_trabalho_db/main.py
+++ TKinter_trabalho_db/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 
 janela = Tk()
 
@@ -8,6 +9,7 @@ class aplicacao():
         self.tela()
         self.frames_da_tela()
         self.widgets_frame_1()
+        self.lista_frame_2()
         janela.mainloop()
     
     def tela(self):
@@ -42,34 +44,53 @@ class aplicacao():
         self.bt_apagar.place(relx= 0.6, rely= 0.1, relwidth= 0.1, relheight= 0.15)
 
         # Criação label entrada do codigo
-        self.lb_codigo = Label(self.frame_1, text= 'Código')
+        self.lb_codigo = Label(self.frame_1, text= 'Código',bg= '#d9dbde',fg='#496487')
         self.lb_codigo.place(relx= 0.05, rely= 0.05)
 
         self.codigo_entry = Entry(self.frame_1)
         self.codigo_entry.place(relx= 0.05, rely= 0.15, relwidth= 0.08)
 
         # Criação label entrada do nome
-        self.lb_nome = Label(self.frame_1, text= 'Nome')
+        self.lb_nome = Label(self.frame_1, text= 'Nome',bg= '#d9dbde',fg='#496487')
         self.lb_nome.place(relx= 0.05, rely= 0.3)
 
         self.nome_entry = Entry(self.frame_1)
-        self.nome_entry.place(relx= 0.05, rely= 0.4, relwidth= 0.8)
+        self.nome_entry.place(relx= 0.05, rely= 0.4, relwidth= 0.9)
 
         # Criação label entrada do telefone
-        self.lb_telefone = Label(self.frame_1, text= 'Telefone')
+        self.lb_telefone = Label(self.frame_1, text= 'Telefone',bg= '#d9dbde',fg='#496487')
         self.lb_telefone.place(relx= 0.05, rely= 0.55)
 
         self.codigo_entry = Entry(self.frame_1)
         self.codigo_entry.place(relx= 0.05, rely= 0.65, relwidth= 0.4)
 
         # Criação label entrada do Cidade
-        self.lb_cidade = Label(self.frame_1, text= 'Cidade')
+        self.lb_cidade = Label(self.frame_1, text= 'Cidade',bg= '#d9dbde',fg='#496487')
         self.lb_cidade.place(relx= 0.05, rely= 0.55)
 
         self.codigo_entry = Entry(self.frame_1)
         self.codigo_entry.place(relx= 0.05, rely= 0.65, relwidth= 0.4)
         
+    def lista_frame_2(self):
+        self.lista_cliente = ttk.Treeview(self.frame_2, height= 3, column= ('col1','col2','col3','col4'))
+        self.lista_cliente.heading('#0', text='')
+        self.lista_cliente.heading('#1', text='Codigo')
+        self.lista_cliente.heading('#1', text='Nome')
+        self.lista_cliente.heading('#1', text='Telefone')
+        self.lista_cliente.heading('#1', text='Cidade')
+
+        self.lista_cliente.column('#0', width=1)
+        self.lista_cliente.column('#1', width=50)
+        self.lista_cliente.column('#2', width=200)
+        self.lista_cliente.column('#3', width=125)
+        self.lista_cliente.column('#4', width=125)
+
+        self.lista_cliente.place(relx=0.01, rely=0.1, relwidth=0.95, relheight=0.85)
+        self.barra_de_rolagem_lista = Scrollbar(self.frame_2, orient='vertical')
+        self.lista_cliente.configure(yscrooll=self.barra_de_rolagem_lista.set)
+        self.barra_de_rolagem_lista.place(relx=0.96, rely=0.1, relwidth=0.04, relheight=0.85)
         
-        
+
+
 
 aplicacao()
